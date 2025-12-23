b2ee1e025c4b4cac10e66be2c0fcdc5562e56d99
Hono <suinaixiangaocun@gmail.com>
2025-12-23 17:52:06 +0900

    fix: 勤怠・休憩Factoryの時間整合性と責務分離を修正

--- src/database/factories/BreakLogFactory.php
+++ src/database/factories/BreakLogFactory.php
@@ -3,6 +3,7 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Carbon\Carbon;
 
 class BreakLogFactory extends Factory
 {
@@ -13,13 +14,43 @@ class BreakLogFactory extends Factory
      */
     public function definition()
     {
-        $start = $this->faker->time('H:i:s', '12:00:00');
-        $end = $this->faker->time('H:i:s', '13:00:00');
-
         return [
-            'attendance_id' => \App\Models\Attendance::factory(),
-            'break_start' => $start,
-            'break_end' => $end,
+            'attendance_id' => null,
+            'break_start'   => null,
+            'break_end'     => null,
         ];
+    }
+
+    public function withinAttendance()
+    {
+        return $this->afterMaking(function ($breakLog) {
+            $attendance = $breakLog->attendance;
+
+            if (!$attendance) {
+                return;
+            }
+
+            $clockIn  = Carbon::parse($attendance->clock_in);
+            $clockOut = Carbon::parse($attendance->clock_out);
+
+            $range = $clockOut->diffInMinutes($clockIn);
+
+            if ($range < 120) {
+                return;
+            }
+
+            $breakStart = (clone $clockIn)
+                ->addMinutes(rand(60, $range - 60));
+
+            $breakEnd = (clone $breakStart)
+                ->addMinutes(rand(30, 60));
+
+            if ($breakEnd->gte($clockOut)) {
+                $breakEnd = (clone $clockOut)->subMinute();
+            }
+
+            $breakLog->break_start = $breakStart;
+            $breakLog->break_end   = $breakEnd;
+        });
     }
 }
