65bbd5c8543315974fd85131a356976bd1dd0954
Hono <suinaixiangaocun@gmail.com>
2025-12-25 16:42:30 +0900

    feat: 一般ユーザー・管理者共通認証ミドルウェアを追加

--- src/app/Http/Kernel.php
+++ src/app/Http/Kernel.php
@@ -63,5 +63,6 @@ class Kernel extends HttpKernel
         'signed' => \Illuminate\Routing\Middleware\ValidateSignature::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
+        'auth.user_or_admin' => \App\Http\Middleware\AuthenticateUserOrAdmin::class,
     ];
 }
